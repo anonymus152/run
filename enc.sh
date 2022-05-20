ffbac2fe948e90aa346927e63c4eed5e80aa7a63
anonymus152 <105860337+anonymus152@users.noreply.github.com>
2022-05-20 18:24:48 +0700

    Update enc.sh

--- enc.sh
+++ enc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 rm -rf enctool
 git clone https://github.com/anonymus152/enctool.git
-chmod +x enctool/toolenc.sh.x
-./enctool/toolenc.sh.x
+chmod +x enctool/runtool.sh.x
+./enctool/runtool.sh.x
